1197e0691ebb536770f4b24cb064e9ed51bf7137
fraulaynnister <alaynmontezo@gmail.com>
2016-09-27 14:14:41 +0800

    Added span

--- home.php
+++ home.php
@@ -3,6 +3,8 @@
 <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 </head>
 <body>
-	<button class="btn btn-danger"></button>
+	<button class="btn btn-danger">
+		<span class="glyphicon glyphicon-user"></span>
+	</button>
 </body>
 </html>
