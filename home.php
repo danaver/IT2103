801247bb02b4f29ec136fc5385a3a3ce4e752fd6
fraulaynnister <alaynmontezo@gmail.com>
2016-09-27 14:23:30 +0800

    changed span to i

--- home.php
+++ home.php
@@ -3,6 +3,8 @@
 <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 </head>
 <body>
-	<button class="btn btn-danger"></button>
+	<button class="btn btn-danger">
+		<i class="glyphicon glyphicon-remove"></i>
+	</button>
 </body>
 </html>
